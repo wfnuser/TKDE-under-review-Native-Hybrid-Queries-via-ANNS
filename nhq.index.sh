320ad27cd86074284d1818bdf3c09bc6cbd0ee37
wfnuser <wfnuser@hotmail.com>
2024-06-28 17:17:50 +0800

    add shell to run index and build

---
+++ nhq.index.sh
@@ -0,0 +1,1 @@
+./NHQ-NPG_nsw/examples/cpp/index ~/datasets/sift-161m/sift/sift_base.fvecs ~/datasets/sift-161m/sift_label/label_sift_base.txt ~/datasets/sift-161m/graph ~/datasets/sift-161m/attrtable 40 100
---
+++ nhq.search.sh
@@ -0,0 +1,1 @@
+./NHQ-NPG_nsw/examples/cpp/search ~/datasets/sift-161m/graph ~/datasets/sift-161m/attrtable ~/datasets/sift-161m/sift/sift_query.fvecs ~/datasets/sift-161m/label_sift_groundtruth.ivecs ~/datasets/sift-161m/sift_label/label_sift_query.txt
