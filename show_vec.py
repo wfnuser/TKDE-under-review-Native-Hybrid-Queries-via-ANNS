83aa7e30073cc6449cda4a60ed823e66db5f5dd8
wfnuser <wfnuser@hotmail.com>
2024-06-30 16:31:27 +0800

    support read fvecs in show_vec.py

--- show_vec.py
+++ show_vec.py
@@ -18,6 +18,24 @@ def read_ivecs(file_path):
             data.append(vector)
         return np.array(data)
 
+def read_fvecs(file_path):
+    with open(file_path, 'rb') as f:
+        data = []
+        i = 0
+        while True:
+            dim_bytes = f.read(4)
+            if not dim_bytes:
+                break
+            i = i + 1
+            if i > 10:
+                break
+            dim = np.frombuffer(dim_bytes, dtype=np.int32)[0]
+            vector = np.frombuffer(f.read(4 * dim), dtype=np.float32)
+            print(len(vector))
+            print(vector)
+            data.append(vector)
+        return np.array(data)
+
 # label_sift_groundtruth = '/home/qinghao/datasets/sift-161m/sift_label/label_sift_groundtruth3.ivecs'
 # data = read_ivecs(label_sift_groundtruth)
 # print("ground_truth", data)
@@ -28,5 +46,5 @@ def read_ivecs(file_path):
 # print("sift_base", data)
 
 sift_query = '/home/qinghao/datasets/sift-161m/sift/sift_query.fvecs'
-data = read_ivecs(sift_query)
+data = read_fvecs(sift_query)
 
